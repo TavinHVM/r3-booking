1ddf2b9be425d8b20b64ce1e0c2dd4cd81f6ac96
TavinHVM <gustav0.henriqu31504@gmail.com>
2025-07-02 17:44:35 -0300

    feat: dark and light mode switch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import { Calendar } from "@/components/ui/calendar";
 import "./globals.css";
+import { ModeToggle } from "@/components/ui/mode-toggle";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-       <h1 className="text-4xl font-bold">R3 Agendamento</h1>
-       <div className="w-full max-w-2xl items-center justify-items-center">
+      <div className="flex items-center justify-center w-full">
+        <ModeToggle />
+        <span className="text-4xl font-bold items-center justify-center ml-2">R3 Agendamento</span>
+      </div>
+      <div className="w-full max-w-2xl items-center justify-items-center">
         <Calendar captionLayout="dropdown" />
       </div>
-     </div>
+    </div>
   );
 }
