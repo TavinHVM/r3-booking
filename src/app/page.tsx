54b82ce019462a3886a1abfb6c6c1c03edb1bc8d
TavinHVM <gustav0.henriqu31504@gmail.com>
2025-07-04 17:32:32 -0300

    feat: font change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
       </div>
 
       <h1 className="w-full text-center text-4xl font-bold mb-2">Agendamento de Reuniões</h1>
-      <div className="w-full items-center justify-items-center min-h-screen p-8 pb-20 gap-4 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="w-full items-center justify-items-center min-h-screen p-8 pb-20 gap-4 sm:p-20 font-[family-name:var(--font-inter)]">
         <Calendar className="w-full"
           mode="single"
           selected={selectedDay}
